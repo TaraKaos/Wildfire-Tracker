240ca2de46bd487d22c9847bf3333549cec68297
tarakaos <taranicoleford@gmail.com>
2020-12-01 01:37:30 -0700

    Implemented fire icons, fire info box, and header

--- src/components/map.js
+++ src/components/map.js
@@ -1,6 +1,19 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './locationMarker'
+import LocationInfoBox from './locationInfoBox'
 
-const Map = ({ center, zoom}) => {
+const Map = ({ eventData, center, zoom}) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
+    const markers = eventData.map(ev => {
+        if(ev.categories[0].id === 8) {
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} 
+            onClick={() => setLocationInfo({id: ev.id, title: ev.title})} />
+        }
+        return null
+    })
+
     return (
         <div className="map">
             <GoogleMapReact
@@ -8,17 +21,18 @@ const Map = ({ center, zoom}) => {
                 defaultCenter={ center }
                 defaultZoom={ zoom }
             >
-
+                {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
 
 Map.defaultProps = {
     center: {
-        lat: 46.8721,
-        lng: -113.9940
+        lat: 46.8797,
+        lng: -110.3626
     },
-    zoom: 7
+    zoom: 6
 }
 export default Map
